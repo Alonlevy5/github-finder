73a9e2218a2121928f4efe60b032bee0b53d5739
alonlevy5 <alonlevy5@gmail.com>
2022-05-27 21:49:42 +0300

    useContext

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,36 +1,27 @@
-import { useEffect, useState } from "react";
-import Spinner from "../layout/Spinner";
-import UserItem from "./UserItem";
+import { useContext, useEffect } from 'react'
+import Spinner from '../layout/Spinner'
+import UserItem from '../users/UserItem'
+import GithubContext from '../../context/github/GithubContext'
 
-export default function UserResults() {
-  const [users, setUsers] = useState([]);
-  const [loading, setLoading] = useState(true);
+function UserResults() {
 
-  useEffect(() => {
-    fetchUsers();
-  }, []);
+    const {users, loading, fetchUsers} = useContext(GithubContext)
 
-  const fetchUsers = async () => {
-    const response = await fetch(`${process.env.REACT_APP_GITHUB_URL}/users`, {
-      headers: {
-       // Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
-      },
-    });
+ useEffect(() => {
+     fetchUsers()
+ }, [])
 
-    const data = await response.json();
-    setUsers(data);
-    setLoading(false);
-  }
-  if(!loading){
+  if (!loading) {
     return (
-        <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
-          {users.map((user) => (
-            <UserItem key={user.id} user={user} />
-          ))}
-        </div>
-      );
-  }else {
-      return <Spinner />
+      <div className='grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2'>
+        {users.map((user) => (
+          <UserItem key={user.id} user={user} />
+        ))}
+      </div>
+    )
+  } else {
+    return <Spinner />
   }
-
 }
+
+export default UserResults
